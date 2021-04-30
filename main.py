b58654d5dbd59076f879472b0400d5c7a508d6d4
Paula-18 <paunahappy@gmail.com>
2021-04-30 13:01:15 -0700

    alexa

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import speech_recognition as sr
 import time 
 import webbrowser
+import playsound
+import os
+import random
+from gtts import gTTS
 from time import ctime
 
 r = sr.Recognizer()
@@ -15,29 +19,44 @@ def record_audio(ask = False):
             voice_data = r.recognize_google(audio)
 
         except sr.UnknownValueError:
-            print('Sorry, I do not understand')
+            alexa_speak('Sorry, I do not understand')
         except sr.RequestError:
-            print('Sorry, connection error')
+            alexa_speak('Sorry, connection error')
         return voice_data
+
+def alexa_speak(audio_string):
+    tts = gTTS(text=audio_string, lang='en')
+    r = random.randint(1, 10000)
+    audio_file = 'audio-' + str(r) + '.mp3'
+    tts.save(audio_file)
+    playsound.playsound(audio_file)
+    print(audio_string)
+    os.remove(audio_file)
 
 def respond(voice_data):
     if 'what is your name' in voice_data:
-        print('My name Alther EGO')
+        alexa_speak('My name is Alther EGO')
     if 'what time is it' in voice_data:
-        print(ctime())
+        alexa_speak(ctime())
+    if 'what is your favorite animal' in voice_data:
+        alexa_speak('My favorite animal is cat')
+    if 'what is your favorite song' in voice_data:
+        alexa_speak('My favorite song is Lemon')
     if 'search' in voice_data:
         buscar = record_audio('what do you search?')
         url = 'https://www.google.com/search?q=' + buscar
         webbrowser.get().open(url)
-        print('This is what I found: ' + buscar)
+        alexa_speak('This is what I found: ' + buscar)
     if 'place' in voice_data:
         lugar = record_audio('what place?')
         url = 'https://www.google.nl/maps/place/' + lugar + '/&amp'
         webbrowser.get().open(url)
-        print('This is what I found: ' + lugar)
+        alexa_speak('This is what I found: ' + lugar)
+    if 'finish' in voice_data:
+        exit()
 
 time.sleep(1)
-print('how can I help you?')
+alexa_speak('can I help you?')
 while 1:
     voice_data = record_audio()
     respond(voice_data)
